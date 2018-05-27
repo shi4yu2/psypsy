413190e0fbd5fdda4898c2f6d67eb59bf0429208
Shi YU <39440960+shi4yu2@users.noreply.github.com>
2018-05-27 21:21:27 +0200

    Add files via upload

---
+++ libpsypsy/psypsyio.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+PSYchology Python by Shi Yu
+I/O handler
+"""
+
+# Todo :
+# * writing output file functions
+
+__author__ = 'ShY'
+__copyright__ = 'Copyright 2018, SHY'
+__version__ = '0.0.1 (20180527)'
+__maintainer__ = 'ShY, Pierre Halle'
+__email__ = 'shi4yu2@gmail.com'
+__status__ = 'Development'
+
+import re
+from openpyxl import load_workbook
+
+
+# ====================== Input =========================
+# ======================================================
+# Processing text files
+# This function returns a dictionary of stimuli
+# containing keys:
+#     header, header_length, conditions, trial_number
+def processing_stimuli_file(filename, separator='\t', header=True):
+    # type: (str, str, bool) -> dict
+    """
+    :param filename: input file name
+    :type: filename: str
+    :param separator: separator of text file
+    :type: separator: str
+    :param header: if header line exists
+    :type: bool
+    :return: output_stimuli_dictionary: dictionary of stimuli
+    :rtype: dict
+    """
+    f = open(filename, mode='rU', encoding="utf8")
+    plain_text = f.read()
+    line_buffer = cleanup_line_breaks(plain_text, regex='[\n|\r]+')
+    f.close()
+
+    # Handle header
+    list_header = filter_empty_item(line_buffer[0].split(separator))
+    header_length = len(list_header)
+
+    if not header:  # when file does not contain a header
+        list_header = list(range(header_length))
+
+    # Create a dictionary containing a list of header items and the length of header
+    output_stimuli_dictionary = {'header': list_header, 'header_length': header_length}
+
+    # Create header conditions in output dictionary
+    header_index = {}
+    for condition in list_header:
+        index = list_header.index(condition)
+        header_index[index] = condition
+        output_stimuli_dictionary[condition] = []
+
+    # Processing list of stimuli
+    if header:
+        start_line = 1
+    else:
+        start_line = 0
+
+    # Fill the dictionary of stimuli
+    for line in line_buffer[start_line:]:
+        list_line = filter_empty_item(line.split('\t'))
+        for item in range(header_length):
+            output_stimuli_dictionary[header_index[item]].append(list_line[item])
+
+    # Compute number of trials
+    trial_number = len(output_stimuli_dictionary[list_header[1]])
+    output_stimuli_dictionary['trial_number'] = trial_number
+
+    return output_stimuli_dictionary
+
+
+# Filter special line break characters =================
+def cleanup_line_breaks(text, regex='[\n|\r]+'):
+    # type: (str, str) -> list
+    """
+    :param text: string of content of input file
+    :type: text: str
+    :param regex: regular expression for line breaks
+    :type: regex: str
+    :return: line_buffer: list of strings
+    :rtype: list
+    """
+    re_line_break = re.compile(regex)  # use regex to filter line breaks
+    line_buffer = re_line_break.split(text)  # chunk text into a list of lines
+    line_buffer = filter_empty_item(line_buffer)  # remove empty lines
+
+    return line_buffer
+
+
+# Filter empty items in a list
+def filter_empty_item(list_origin):
+    # type: (list) -> list
+    """
+    :param list_origin: original list containing empty items
+    :type: list_origin: list
+    :return: list without empty items
+    """
+
+    return list(filter(None, list_origin))
+
+
+# Read excel files and returns a dictionary of stimuli
+# containing keys:
+#     header, header_length, conditions, trial_number
+def processing_stimuli_file_excel(filename):
+    # type: (str) -> dict
+    """
+    Processing excel_file input
+    :param filename: Excel file
+    :type filename: str
+    :returns: dictionary of stimuli
+    :rtype: dict
+    """
+    output_stimuli_dictionary = {}
+    title_sheet = []
+    wb = load_workbook(filename=filename, read_only=True)
+
+    # header
+    first_sheet = wb.get_sheet_names()[0]
+    ws = wb.get_sheet_by_name(first_sheet)
+    first_row_ws = ws[1]
+    for item in first_row_ws:
+        title_sheet.append(item.value)
+    header_length = len(title_sheet)
+
+    output_stimuli_dictionary['header_length'] = header_length  # get header_length
+    output_stimuli_dictionary['header'] = title_sheet
+
+    # stimuli
+    # empty dictionary with conditions
+    for condition in title_sheet:
+        output_stimuli_dictionary[str(condition)] = []
+
+    for row in ws.iter_rows(row_offset=1):
+        for cell in row:
+            file_column = cell.column
+            title_column = ws.cell(row=1, column=file_column).value
+            output_stimuli_dictionary[title_column].append(cell.value.decode('utf-8'))
+
+    # get number of trials
+    trial_number = ws.max_row - 1
+    output_stimuli_dictionary['trial_number'] = trial_number
+
+    return output_stimuli_dictionary
+
+
+def read_stimuli(filename, separator='\t', header=True):
+    # type: (str, str, bool) -> dict
+    """
+    Processing stimuli input
+    :param filename: file (text or excel)
+    :type filename: str
+    :param separator: separator of column
+    :type separator: str
+    :param header: if header line exists
+    :type header: bool
+    :returns: dictionary of stimuli
+    :rtype: dict
+    """
+    if filename.endswith('.xlsx'):
+        return processing_stimuli_file_excel(filename)
+    else:
+        return processing_stimuli_file(filename, separator, header)
+
+# ====================== Input =========================
+
+
+# ====================== Output ========================
+# ======================================================
+def write_result():
+    pass
